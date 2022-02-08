3851a812dc5228a08633617fc729a4532ffc4468
eurico77 <euricomagalhaes61@gmail.com>
2022-02-08 20:40:50 -0300

    (chore): run lints and prettier for pattern writing codes

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,7 @@ import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
-
 export class SetupServer extends Server {
-
   constructor(private port = 3000) {
     super();
   }
@@ -21,13 +19,12 @@ export class SetupServer extends Server {
     this.app.use(bodyParser.json());
   }
 
-
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
   }
 
   public getApp(): Application {
-    return this.app
+    return this.app;
   }
 }
